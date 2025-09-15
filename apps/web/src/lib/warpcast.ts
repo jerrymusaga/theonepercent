3aab3f46e5052e199b09a169b66a3bf41873caf7
Jerry Musaga <musagatechnology@gmail.com>
2025-09-15 15:48:08 +0100

    warpcast fix

--- apps/web/src/lib/warpcast.ts
+++ apps/web/src/lib/warpcast.ts
@@ -52,7 +52,7 @@ export async function getFarcasterManifest() {
       splashBackgroundColor: "#FFFFFF",
       webhookUrl: `${appUrl}/api/webhook`,
       // Metadata https://github.com/farcasterxyz/miniapps/discussions/191
-      subtitle: "a multiplayer prediction game", // 30 characters, no emojis or special characters, short description under app name
+      subtitle: "multiplayer prediction game", // 30 characters, no emojis or special characters, short description under app name
       description: "a multiplayer prediction elimination game where minority wins in any round", // 170 characters, no emojis or special characters, promotional message displayed on Mini App Page
       primaryCategory: "social",
       tags: ["mini-app", "celo"], // up to 5 tags, filtering/search tags
