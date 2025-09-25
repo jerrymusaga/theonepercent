b39b910b51201be472fbaf176164b1e49a4050d2
Jerry Musaga <musagatechnology@gmail.com>
2025-09-25 02:34:15 +0100

    update test file

--- apps/indexer/test/test.ts
+++ apps/indexer/test/test.ts
@@ -1,120 +1,86 @@
 import assert from "assert";
-import { TestHelpers, User } from "generated";
-const { MockDb, Greeter, Addresses } = TestHelpers;
+import { TestHelpers } from "generated";
 
-describe("Greeter template tests", () => {
-  it("A NewGreeting event creates a User entity", async () => {
+const { MockDb, CoinToss, Addresses } = TestHelpers;
+
+describe("CoinToss indexer tests", () => {
+  it("A PoolCreated event creates a Pool entity", async () => {
     // Initializing the mock database
     const mockDbInitial = MockDb.createMockDb();
 
     // Initializing values for mock event
-    const userAddress = Addresses.defaultAddress;
-    const greeting = "Hi there";
+    const creatorAddress = Addresses.defaultAddress;
+    const poolId = 1n;
+    const entryFee = 1000000000000000000n; // 1 CELO in wei
+    const maxPlayers = 10n;
 
     // Creating a mock event
-    const mockNewGreetingEvent = Greeter.NewGreeting.createMockEvent({
-      greeting: greeting,
-      user: userAddress,
+    const mockPoolCreatedEvent = CoinToss.PoolCreated.createMockEvent({
+      poolId: poolId,
+      creator: creatorAddress,
+      entryFee: entryFee,
+      maxPlayers: maxPlayers,
     });
 
     // Processing the mock event on the mock database
-    const updatedMockDb = await Greeter.NewGreeting.processEvent({
-      event: mockNewGreetingEvent,
+    const updatedMockDb = await CoinToss.PoolCreated.processEvent({
+      event: mockPoolCreatedEvent,
       mockDb: mockDbInitial,
     });
 
-    // Expected entity that should be created
-    const expectedUserEntity: User = {
-      id: userAddress,
-      latestGreeting: greeting,
-      numberOfGreetings: 1,
-      greetings: [greeting],
-    };
+    // Getting the pool entity from the mock database
+    const actualPoolEntity = updatedMockDb.entities.Pool.get(poolId.toString());
 
-    // Getting the entity from the mock database
-    const actualUserEntity = updatedMockDb.entities.User.get(userAddress);
-
-    // Asserting that the entity in the mock database is the same as the expected entity
-    assert.deepEqual(expectedUserEntity, actualUserEntity);
+    // Basic assertions about the created pool
+    assert.equal(actualPoolEntity?.id, poolId.toString());
+    assert.equal(actualPoolEntity?.creator, creatorAddress);
+    assert.equal(actualPoolEntity?.entryFee, entryFee);
+    assert.equal(actualPoolEntity?.maxPlayers, Number(maxPlayers));
+    assert.equal(actualPoolEntity?.currentPlayers, 0);
   });
 
-  it("2 Greetings from the same users results in that user having a greeter count of 2", async () => {
-    // Initializing the mock database
+  it("A PlayerJoined event updates the pool and creates player entities", async () => {
+    // Initializing the mock database with a pool
     const mockDbInitial = MockDb.createMockDb();
-    // Initializing values for mock event
-    const userAddress = Addresses.defaultAddress;
-    const greeting = "Hi there";
-    const greetingAgain = "Oh hello again";
+    const creatorAddress = Addresses.defaultAddress;
+    const playerAddress = Addresses.mockAddresses[1];
+    const poolId = 1n;
+    const entryFee = 1000000000000000000n;
+    const maxPlayers = 10n;
 
-    // Creating a mock event
-    const mockNewGreetingEvent = Greeter.NewGreeting.createMockEvent({
-      greeting: greeting,
-      user: userAddress,
+    // First create a pool
+    const mockPoolCreatedEvent = CoinToss.PoolCreated.createMockEvent({
+      poolId: poolId,
+      creator: creatorAddress,
+      entryFee: entryFee,
+      maxPlayers: maxPlayers,
     });
 
-    // Creating a mock event
-    const mockNewGreetingEvent2 = Greeter.NewGreeting.createMockEvent({
-      greeting: greetingAgain,
-      user: userAddress,
-    });
-
-    // Processing the mock event on the mock database
-    const updatedMockDb = await Greeter.NewGreeting.processEvent({
-      event: mockNewGreetingEvent,
+    const mockDbWithPool = await CoinToss.PoolCreated.processEvent({
+      event: mockPoolCreatedEvent,
       mockDb: mockDbInitial,
     });
 
-    // Processing the mock event on the updated mock database
-    const updatedMockDb2 = await Greeter.NewGreeting.processEvent({
-      event: mockNewGreetingEvent2,
-      mockDb: updatedMockDb,
+    // Then add a player
+    const mockPlayerJoinedEvent = CoinToss.PlayerJoined.createMockEvent({
+      poolId: poolId,
+      player: playerAddress,
+      currentPlayers: 1n,
+      maxPlayers: maxPlayers,
     });
 
-    // Getting the entity from the mock database
-    const actualUserEntity = updatedMockDb2.entities.User.get(userAddress);
-
-    // Asserting that the field value of the entity in the mock database is the same as the expected field value
-    assert.equal(2, actualUserEntity?.numberOfGreetings);
-  });
-
-  it("2 Greetings from the same users results in the latest greeting being the greeting from the second event", async () => {
-    // Initializing the mock database
-    const mockDbInitial = MockDb.createMockDb();
-    // Initializing values for mock event
-    const userAddress = Addresses.defaultAddress;
-    const greeting = "Hi there";
-    const greetingAgain = "Oh hello again";
-
-    // Creating a mock event
-    const mockNewGreetingEvent = Greeter.NewGreeting.createMockEvent({
-      greeting: greeting,
-      user: userAddress,
+    const updatedMockDb = await CoinToss.PlayerJoined.processEvent({
+      event: mockPlayerJoinedEvent,
+      mockDb: mockDbWithPool,
     });
 
-    // Creating a mock event
-    const mockNewGreetingEvent2 = Greeter.NewGreeting.createMockEvent({
-      greeting: greetingAgain,
-      user: userAddress,
-    });
+    // Check that pool was updated
+    const updatedPool = updatedMockDb.entities.Pool.get(poolId.toString());
+    assert.equal(updatedPool?.currentPlayers, 1);
 
-    // Processing the mock event on the mock database
-    const updatedMockDb = await Greeter.NewGreeting.processEvent({
-      event: mockNewGreetingEvent,
-      mockDb: mockDbInitial,
-    });
-
-    // Processing the mock event on the updated mock database
-    const updatedMockDb2 = await Greeter.NewGreeting.processEvent({
-      event: mockNewGreetingEvent2,
-      mockDb: updatedMockDb,
-    });
-
-    // Getting the entity from the mock database
-    const actualUserEntity = updatedMockDb2.entities.User.get(userAddress);
-
-    const expectedGreeting: string = greetingAgain;
-
-    // Asserting that the field value of the entity in the mock database is the same as the expected field value
-    assert.equal(expectedGreeting, actualUserEntity?.latestGreeting);
+    // Check that player was created
+    const player = updatedMockDb.entities.Player.get(playerAddress);
+    assert.equal(player?.address, playerAddress);
+    assert.equal(player?.totalPoolsJoined, 1);
   });
 });
